cd2e4ae713bf3edf893b00462202109d9dcb4e86
Rogue Andy <zlj_1969_love@126.com>
2017-07-19 14:35:47 +0800

    解决 admin.jade 缩进导致的页面不正常问题

--- NodeWeb/app.js
+++ NodeWeb/app.js
@@ -81,46 +81,46 @@ app.get('/admin/update:id', function(req, res) {
 });
 
 app.post('/admin/movie/new', function(req, res) {
-  console.log('wwwwwwwww');
-  // return;
-  // var id = req.body.movie._id;
-  // var movieObj = req.body.movie;
-  // var _movie;
-  //
-  // if(id != 'undefined') {
-  //   Movie.findById(id, function(err, movie) {
-  //     if(err) {
-  //       console.log(err);
-  //     }
-  //
-  //     _movie = _.extend(movie, movieObj);
-  //     _movie.save(function(err, movie) {
-  //       if(err) {
-  //         console.log(err);
-  //       }
-  //       res.redirect('/movie/' + movie._id);
-  //     });
-  //   });
-  // } else {
-  //   _movie = new Movie({
-  //     doctor: movieObj.doctor,
-  //     title: movieObj.title,
-  //     country: movieObj.country,
-  //     language: movieObj.language,
-  //     year: movieObj.year,
-  //     poster: movieObj.poster,
-  //     summary: movieObj.summary,
-  //     flash: movieObj.flash
-  //   });
-  //
-  //   _movie.save(function(err, movie) {
-  //     if(err) {
-  //       console.log(err);
-  //     }
-  //
-  //     res.redirect('/movie/' + movie._id);
-  //   });
-  // }
+
+  var id = req.body.movie._id;
+  var movieObj = req.body.movie;
+  var _movie;
+
+  if(id != 'undefined') {
+    Movie.findById(id, function(err, movie) {
+      if(err) {
+        console.log(err);
+      }
+
+      _movie = _.extend(movie, movieObj);
+      _movie.save(function(err, movie) {
+        if(err) {
+          console.log(err);
+        }
+        res.redirect('/movie/' + movie._id);
+      });
+    });
+  } else {
+    _movie = new Movie({
+      doctor: movieObj.doctor,
+      title: movieObj.title,
+      country: movieObj.country,
+      language: movieObj.language,
+      year: movieObj.year,
+      poster: movieObj.poster,
+      summary: movieObj.summary,
+      flash: movieObj.flash
+    });
+
+    _movie.save(function(err, movie) {
+      if(err) {
+        console.log(err);
+      }
+
+      res.redirect('/movie/' + movie._id);
+    });
+  }
+
 });
 
 app.get('/admin/list', function(req, res) {
