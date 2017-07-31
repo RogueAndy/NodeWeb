e15d5868a9973445098e88c60a8e41d6d8c77c30
Rogue Andy <zlj_1969_love@126.com>
2017-07-31 10:05:22 +0800

    使用正则表达式来过滤字符串，模糊查询数据库

--- NodeWeb/app/controllers/index.js
+++ NodeWeb/app/controllers/index.js
@@ -27,30 +27,54 @@ exports.index = function (req, res) {
 exports.search = function (req, res) {
 	
   var catId = req.query.cat;
-  var page = parseInt(req.query.p, 10);
+  var q = req.query.q;
+  var page = parseInt(req.query.p, 10) || 0;
   var count = 5;
   var index = page * count;
   
-	Category
-		.find({_id: catId})
-		.populate({path: 'movies', select: 'title poster'})
-		.exec(function (err, categories) {
-			if(err) {
-				console.log(err);
-			}
-   
-			var category = categories[0] || {};
-			var movies = category.movies || [];
-			var results = movies.slice(index, index + count);
+  if(catId) {
+    Category
+    .find({_id: catId})
+    .populate({path: 'movies', select: 'title poster'})
+    .exec(function (err, categories) {
+      if(err) {
+        console.log(err);
+      }
 
-			res.render('results', {
-				title: 'imooc 搜索结果列表',
-				keyword: category.name,
-				currentPage: (page + 1),
-				query: 'cat=' + catId,
-				totalPage: Math.ceil(movies.length / count),
-				movies: results
-			});
-		});
-	
+      var category = categories[0] || {};
+      var movies = category.movies || [];
+      var results = movies.slice(index, index + count);
+
+      res.render('results', {
+        title: 'imooc 搜索结果列表',
+        keyword: category.name,
+        currentPage: (page + 1),
+        query: 'cat=' + catId,
+        totalPage: Math.ceil(movies.length / count),
+        movies: results
+      });
+    });
+  }
+  else {
+    var par = {title: new RegExp(q)};
+    Movie
+    .find(par)
+    .exec(function (err, movies) {
+      if(err) {
+        console.log(err);
+      }
+
+      var results = movies.slice(index, index + count);
+
+      res.render('results', {
+        title: 'imooc 结果列表页面',
+        keyword: q,
+        currentPage: (page + 1),
+        query: 'q=' + q,
+        totalPage: Math.ceil(movies.length / count),
+        movies: results
+      });
+    });
+  }
+
 };
